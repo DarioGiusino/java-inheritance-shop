c7a533f6f796a4be115f08703bda2130d6d95d3d
DarioGiusino <dario.giusino@gmail.com>
2023-05-09 15:07:46 +0200

    smartphone class

---
+++ src/org/java/lessons/inheritance/Smartphone.java
@@ -0,0 +1,35 @@
+package org.java.lessons.inheritance;
+
+public class Smartphone extends Prodotto {
+	
+	private String IMEI;
+	private int memory;
+	
+	public Smartphone(int code, String label, String brand, double price, int vat, String IMEI, int memory) {
+		super(code, label, brand, price, vat);
+		setIMEI(IMEI);
+		setMemory(memory);
+	}
+
+	public String getIMEI() {
+		return IMEI;
+	}
+	public void setIMEI(String iMEI) {
+		IMEI = iMEI;
+	}
+
+	public int getMemory() {
+		return memory;
+	}
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	
+	@Override
+	public String toString() {
+		return super.getDescription()
+				+ "\nCodice IMEI: " + getIMEI()
+				+ "\nQuantità di memoria: " + getMemory() + "GB";
+	}
+
+}
