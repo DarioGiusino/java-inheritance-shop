50d1b50a0d514bc54ec12d84bfd49677dda30226
DarioGiusino <dario.giusino@gmail.com>
2023-05-09 14:51:34 +0200

    first commit

---
+++ src/org/java/lessons/inheritance/Prodotto.java
@@ -0,0 +1,11 @@
+package org.java.lessons.inheritance;
+
+public class Prodotto {
+	
+	private int code;
+	private String label;
+	private String brand;
+	private double price;
+	private int vat;
+	
+}
